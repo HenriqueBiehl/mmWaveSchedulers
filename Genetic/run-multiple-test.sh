121645af3ad4dede8aa90796870e7b3945e14e2d
Raphael Kaviak <rkmach17@gmail.com>
2026-07-04 18:58:15 -0300

    [add] Testes e resultados do BILP e Genético sem divisão

--- Genetic/run-multiple-test.sh
+++ Genetic/run-multiple-test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-runs=2
+runs=30
 total_time=0
 total_fitness=0
-# time_limits=(1 2 4 1000000000000)
-# population_sizes=(5 10 30 50 100)
-# generation_sizes=(30000 50000 100000)
-# mutation_types=(0.15 0.30)
-
 time_limits=(1 2 4 1000000000000)
-population_sizes=(5)
-generation_sizes=(100000)
-mutation_types=(0.30)
+population_sizes=(5 10 30)
+generation_sizes=(30000 50000 100000)
+mutation_types=(0.15 0.30)
+
+#time_limits=(1 2 4 1000000000000)
+#population_sizes=(5)
+#generation_sizes=(100000)
+#mutation_types=(0.30)
 DIR="./Results-DIV/"
 
 if [ -z "$1" ]; then
